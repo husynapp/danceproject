bc5d17a0d544cc89ee6b9e4d63fb9152260d352d
Abdullah Hussain <hussaa94@outlook.com>
2018-11-14 11:26:54 +0000

    New project start

---
+++ DanceGenerator/datatypes/Queue.java
@@ -0,0 +1,35 @@
+package datatypes;
+
+public class Queue<T> implements QueueADT<T>{
+
+	@Override
+	public void enqueue(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
